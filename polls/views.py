a1d6fb72bf0b81621f42f96bfe03989d3a7a7077
user <ttylets@mail.ru>
2023-04-12 21:56:52 +0300

    Initial commit

--- polls/views.py
+++ polls/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpRequest, Http404
 
-from .models import Question
+from .models import Question, Choice
 
 
 def index(request: HttpRequest):
-    questions = Question.objects.order_by('-pub_date')[:5]
+    questions = Question.objects.order_by('-pub_date')[:10]
     context = {'latest_question_list': questions}
     return render(request, 'polls/index.html', context)
 
@@ -14,4 +14,32 @@ def detail(request, question_id: int):
     question = get_object_or_404(Question, id=question_id)
     context = {'question': question}
     return render(request, 'polls/detail.html', context)
+
+
+def vote(request, question_id: int):
+    question = get_object_or_404(Question, id=question_id)
+    try:
+        selected_choice = question.choices.get(id=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html'), {
+            'error_massage =': "You didn't select a choice",
+            question: 'question',
+        }
+    selected_choice.votes += 1
+    selected_choice.save()
+    return redirect('polls:results', question.id)
+
+
+def results(request, question_id: int):
+    question = get_object_or_404(Question, id=question_id)
+    context = {'question' : question}
+    return render(request, 'polls/results.html', context)
+
+
+
+
+
+
+
+
 
